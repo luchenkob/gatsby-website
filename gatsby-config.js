b2af5f752b55047a8fe9ead8791a7f5275f95565
Bogdan Luchenko <luchenkob@gmail.com>
2020-07-03 15:50:05 +0600

    Update gatsby config to match website name

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,9 +5,9 @@ const fullConfig = resolveConfig(tailwindConfig);
 
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Starter Tailwind`,
-    description: `Gatsby starter styled with Tailwind`,
-    author: `@taylorbryant`,
+    title: `Meliorism`,
+    description: `Meliorism`,
+    author: `@knjo`,
   },
   plugins: [
     `gatsby-plugin-eslint`,
